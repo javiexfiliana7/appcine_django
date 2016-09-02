e85fecbcbc1f98264e10e8b162fada77184ae745
javiexfiliana7 <javiexfiliana7@gmail.com>
2016-09-02 17:58:14 +0200

    sube

--- miproyecto/settings.py
+++ miproyecto/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/' #acceso para el servidor web
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = ( #para el local
